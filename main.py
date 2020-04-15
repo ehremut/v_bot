07ce2d81f0dc0d1561a3cf55ec669ae78398b633
ehremut <34132244+ehremut@users.noreply.github.com>
2020-04-15 20:21:35 +0300

    Add files via upload
    -theme from google
    -work easy and hard

--- main.py
+++ main.py
@@ -1,57 +1,32 @@
 import vk_api
-from vk_api.longpoll import VkEventType
-import json
 import random
 import time
-
 from vk_api.longpoll import VkLongPoll
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+credentials_file = 'My First Project-9cc0f18cb40c.json'
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    credentials_file, ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
+client = gspread.authorize(credentials)
+
 
 vk = vk_api.VkApi(token="1bca69c30b5794c55ab9b952909d8acf92f106ca6079a3acbf3c9f6554cea23f47b276e53d609101b369a")
-
 vk._auth_token()
-
 vk.get_api()
-
-easy_theme=["ЭП","ЭПСЧ","ЭПСЧ как","ЭП сожалеет","ЭП откажет"]
-
-hard_theme=["ЭП","ЭПСЧ","ЭПСЧ как","ЭП сожалеет","ЭП откажет"]
-
-def get_button(text,label,payload=""): #
-    return{
-        "action" : text,
-        "payload" : json.dumps(payload),
-        "label": label
-    }
-keyboard = {
-    "one_time" : False,
-    "buttons" : [
-        [get_button("Легкая тема", "green")],
-        [get_button("Сложная тема", "green")]
-    ]
-
-}
-
-keyboard = json.dumps(keyboard, ensure_ascii=False).encode('utf-8')
-keyboard = str(keyboard.decode('utf-8'))
-
 longpoll = VkLongPoll(vk)
 
 def get_theme(type):
+    sheet = client.open_by_url(
+        'https://docs.google.com/spreadsheets/d/1XMy-3kcitEFHIYeA0BMgUN_-z-B5YS2-xGjuqVHkYys/edit#gid=0')
     if type == "easy":
-        return easy_theme[random.randint(1,len(easy_theme)-1)]
+        pack = sheet.worksheet("Простые")
+        data = pack.get_all_values()
+        return data[random.randint(1, len(data) - 1)]
     if type == "hard":
-        return hard_theme[random.randint(1,len(hard_theme)-1)]
-
-
-
-# while True:
-#     for event in longpoll.listen():
-#         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-#             if event.text.lower() == "легкая тема":
-#                 vk.method("messages.send", {"peer_id": event.peer_id, "message": get_theme("easy"), "random_id": 12341234})
-#             if event.text.lower() == "тяжелая тема":
-#                 vk.method("messages.send",
-#                           {"peer_id": event.peer_id, "message": get_theme("hard"), "random_id": 12341234})
+        pack = sheet.get_worksheet("Сложные")
+        data = pack.get_all_values()
+        return data[random.randint(1, len(data) - 1)]
 
 while True:
     try:
@@ -65,6 +40,6 @@ while True:
                 vk.method("messages.send", {"peer_id": id, "message": get_theme("hard"), "random_id": random.randint(1, 2147483647)})
 
             else:
-                vk.method("messages.send", {"peer_id": id, "message": "я не знаю что значит " + str(body), "random_id": random.randint(1, 2147483647)})
+                vk.method("messages.send", {"peer_id": id, "random_id": random.randint(1, 2147483647)}) # "message": "я не знаю что значит " + str(body),
     except Exception as E:
         time.sleep(1)
