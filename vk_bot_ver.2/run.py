911a31703cec657a6c702e11aa5f3da248525ae2
ehremut <34132244+ehremut@users.noreply.github.com>
2020-04-21 20:19:13 +0300

    Fix errors
    The run () function is wrapped in try except so that when the vk servers reboot, the bot does not crash.

--- vk_bot_ver.2/run.py
+++ vk_bot_ver.2/run.py
@@ -22,23 +22,26 @@ def get_name(id):
     return fullname
 
 def run():
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('\n'+get_name(event.user_id) + ' (id: ' + str(event.user_id) + ')')
-            print('Сообщение пришло в: ' + str(datetime.strftime(datetime.now(), "%H:%M:%S")))
-            print('Текст сообщения: ' + str(event.text))
-            print("---------------------------------\n")
-            response = event.text.lower()
-            keyboard = Keyboard.create_keyboard(response)
+    try:
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW:
+                print('\n'+get_name(event.user_id) + ' (id: ' + str(event.user_id) + ')')
+                print('Сообщение пришло в: ' + str(datetime.strftime(datetime.now(), "%H:%M:%S")))
+                print('Текст сообщения: ' + str(event.text))
+                print("---------------------------------\n")
+                response = event.text.lower()
+                keyboard = Keyboard.create_keyboard(response)
 
-            if event.from_user and event.to_me:
-                if event.user_id not in users_bot:
-                    users_bot[event.user_id] = VkBot(event.user_id)
-                bb = users_bot[event.user_id].message(response, keyboard)
-                print(users_bot[event.user_id].send)
-                print(users_bot[event.user_id].steps)
-                if len(bb) == 4 :
-                    send_message(vk,bb[0], bb[1],message=bb[2], keyboard=bb[3])
-                elif len(bb) == 3:
-                    send_message(vk, bb[0], bb[1], message=bb[2])
+                if event.from_user and event.to_me:
+                    if event.user_id not in users_bot:
+                        users_bot[event.user_id] = VkBot(event.user_id)
+                    bb = users_bot[event.user_id].message(response, keyboard)
+                    print(users_bot[event.user_id].send)
+                    print(users_bot[event.user_id].steps)
+                    if len(bb) == 4 :
+                        send_message(vk,bb[0], bb[1],message=bb[2], keyboard=bb[3])
+                    elif len(bb) == 3:
+                        send_message(vk, bb[0], bb[1], message=bb[2])
+    except Exception as E:
+        time.sleep(3)
 
